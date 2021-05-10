7d29d4db619d1e03b5d0ed1bcb501c19cd891985
Raghvendra Dubey <raghavd.dubey22@gmail.com>
2021-05-10 17:49:14 +0530

    Commit 2

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 const path = require("path");
-
+const port = process.env.PORT || 8080
 app.set("view engine","hbs");
 app.get("/",(req,res)=>{
-    res.render('index.hbs');
+    res.render('index.hbs'); 
 });
 app.get("*",(req,res)=>{
     
    res.write("erroe!!");
 });
-app.listen(8090,()=>{
+app.listen(port,()=>{
     console.log("listen");
 });
